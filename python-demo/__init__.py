b5e0b85e893259b8def682ef9685bb4617a05ed5
nikai <keni1006@163.com>
2019-05-17 08:24:26 +0800

    添加类

---
+++ python-demo/__init__.py
@@ -0,0 +1,15 @@
+class A(object):
+    def doSomething(self):
+        print('this is A')
+
+
+class B(A):
+
+    def doSomething(self):
+        print("this is B")
+        super(B, self).doSomething()
+
+
+if __name__ == '__main__':
+    b = B()
+    b.doSomething()
