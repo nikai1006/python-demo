b032790c9061483a58978c17fa829b8bef351378
nikai <854324946@qq.com>
2019-07-03 10:02:58 +0800

    decorator

---
+++ python-demo/function/decorator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Name : decorator.py
+Author : nikai
+Time : 2019/7/3 9:56
+Desc : 修饰器
+"""
+
+
+def aDecorator(f):
+    print("this is deaoctor")
+    return f
+
+
+@aDecorator
+def a(x, y):
+    print(x * y)
+
+
+if __name__ == '__main__':
+    a(4, 7)
